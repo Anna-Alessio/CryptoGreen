3130819553aff1e74c637d706e296f6765fc530a
Annalex16 <anna.aless2@gmail.com>
2025-03-12 23:20:05 +0100

    Update NavLink.jsx

--- src/Components/Molecules/NavLink.jsx
+++ src/Components/Molecules/NavLink.jsx
@@ -15,29 +15,6 @@ export default function NavLink({ text, icon, to = "#" }) {
                 <TextSM className="font-bold uppercase">{text}</TextSM>
             </Link>
 
-            {/* <nav className="bg-accento text-white h-20 rounded-[30px] flex items-center justify-center gap-12">
-                         <Link className='flex flex-col items-center'>
-
-                              <Wallet />
-
-                              <TextSM className="font-bold uppercase">Wallet</TextSM>
-                         </Link>
-                         <Link className='flex flex-col items-center'>
-
-                              <Market />
-
-                              <TextSM className="font-bold uppercase">Market</TextSM>
-                         </Link>
-                         <Link className='flex flex-col items-center'>
-
-                              <Profile />
-
-                              <TextSM className="font-bold uppercase">Profile</TextSM>
-                         </Link>
-                    </nav> */}
-
-
-
         </>
     )
 }
