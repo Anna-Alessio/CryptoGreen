c48336a48b2a98b69854f960b328cfbb612c79a0
Annalex16 <anna.aless2@gmail.com>
2025-03-12 23:23:28 +0100

    Update MainHeader.jsx

--- src/Components/Organisms/MainHeader.jsx
+++ src/Components/Organisms/MainHeader.jsx
@@ -18,25 +18,6 @@ export default function MainHeader({ title, subtitle }) {
             </header>
 
 
-            {/* <div className="h-screen bg-white flex flex-col justify-between p-6">
-                    <header className="flex items-center justify-between">
-                         <div>
-                              <h1 className="text-4xl font-bold">Wallet</h1>
-                              <p className="text-secondario text-xl">
-                                   Welcome, 
-                                    <span className="font-bold text-secondario text-xl"> Name</span>
-                              </p>
-                         </div>
-                        
-                              <ProfileImage></ProfileImage>
-
-                    </header>
-
-               </div> */}
-
-
-
-
         </>
 
     )
